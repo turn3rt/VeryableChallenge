fbc802fc2ec5a058c6a2d7e9d11ab8198b05dbdd
Turner Thornberry <12401998+turn3rt@users.noreply.github.com>
2020-04-28 14:07:35 -0400

    table view prototype working; need to implement JSON call from https://veryable-public-assets.s3.us-east-2.amazonaws.com/veryable.json

--- VeryableDemo/TableViewControllers/Accounts.swift
+++ VeryableDemo/TableViewControllers/Accounts.swift
@@ -22,19 +22,18 @@ class Accounts: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 3
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "accountsCard", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
